f583e3f9b24bd5d4026545d22161abfd4e962fae
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-10 16:35:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -29,32 +29,53 @@
 #   end
 # end
 
+# class School
+
+# attr_accessor :name, :roster
+
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+
+#   def add_student(student_name, grade)
+#     if @roster[grade]
+#       @roster[grade] << student_name
+#     else
+#       @roster[grade] = [student_name]
+#     end
+#   end
+
+#   def grade(grade)
+#     @roster[grade]
+#   end
+
+#   def sort
+#     sorted_hash = {}
+#     @roster.sort.each do |grade, name_array|
+#       sorted_hash[grade] = (name_array.sort)
+#     end
+#     sorted_hash
+#   end
+# end
+
 class School
-
-attr_accessor :name, :roster
-
-  def initialize(name)
-    @name = name
-    @roster = {}
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
   end
 
-  def add_student(student_name, grade)
-    if @roster[grade]
-      @roster[grade] << student_name
-    else
-      @roster[grade] = [student_name]
-    end
-  end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    sorted_hash = {}
-    @roster.sort.each do |grade, name_array|
-      sorted_hash[grade] = (name_array.sort)
-    end
-    sorted_hash
-  end
 end
+
