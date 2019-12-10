7aa9f5bd8f5428a2e9affb03d5bade974e859e18
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-10 16:50:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,11 +7,11 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, grade)
+  def add_student(student, grade)
     if @roster[grade]
-      @roster[grade] << student_name
+      @roster[grade] << student
     else
-      @roster[grade] = [student_name]
+      @roster[grade] = [student]
     end
   end
 
