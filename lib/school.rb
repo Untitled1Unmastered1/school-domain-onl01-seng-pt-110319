8fbff8033226c74ba59f212d56be6f58a87efa1b
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-10 16:10:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,10 @@
 class School
-  attr_accessor :student, : 
+  attr_accessor :student
+  attr_reader :roster 
   
   ROSTER = []
+  
+  def initialize(roster)
+    @roster = roster 
+    ROSTER << 
 end 
