c6f1416e82ee62d52af6675a68ff625fa9d7b75e
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-10 16:19:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,30 @@
 class School
-  attr_accessor :student, :grade 
-  attr_reader :roster 
-  
-  ROSTER = []
-  
-  def initialize(student)
-    @student = student
-  end 
-  
-  def grade=(grade)
-    @grade = grade
-  end 
 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
