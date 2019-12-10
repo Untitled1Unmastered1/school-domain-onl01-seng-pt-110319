1ecb6c43081e6fa8f4543eed2348e319f3c508f3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-10 16:33:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,60 @@
+# class School
+
+#   def initialize(school_name)
+#     @school_name = school_name
+#     @roster = {}
+#   end
+
+#   def roster
+#     @roster
+#   end
+
+#   def add_student(name, grade)
+#     @student_name = name
+#     @grade = grade
+#     if @roster.include?(grade) == false
+#       @roster[grade] = []
+#     end
+#     @roster[grade] << name
+#   end
+
+#   def grade(number)
+#     @roster[number]
+#   end
+
+#   def sort
+#     @roster.each do |grade, name|
+#       @roster[grade] = name.sort
+#     end
+#   end
+# end
+
 class School
 
-  def initialize(school_name)
-    @school_name = school_name
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
     @roster = {}
   end
 
-  def roster
-    @roster
-  end
-
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
     end
-    @roster[grade] << name
   end
 
-  def grade(number)
-    @roster[number]
+  def grade(grade)
+    @roster[grade]
   end
 
   def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+    sorted_hash = {}
+    @roster.sort.each do |grade, name_array|
+      sorted_hash[grade] = (name_array.sort)
     end
+    sorted_hash
   end
 end
